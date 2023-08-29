26705c6b821501470c1ee1fa8e54d37b87c86ea6
RG-HC <al058785@ucam.mx>
2023-08-29 13:56:25 -0600

    Page View

--- lib/presentation/screens/app_tutorial/app_tutorial_screen.dart
+++ lib/presentation/screens/app_tutorial/app_tutorial_screen.dart
@@ -1,10 +1,58 @@
 import 'package:flutter/material.dart';
 
-class AppTutorialScreen extends StatelessWidget {
+class SlideInfo {
+  final String title;
+  final String caption;
+  final String imageUrl;
 
+  SlideInfo(this.title, this.caption, this.imageUrl);
+}
+
+final slides = <SlideInfo>[
+  SlideInfo(
+      'Busca la Comida',
+      'Irure id ad reprehenderit commodo magna voluptate incididunt veniam nulla esse.',
+      'assetes/image/1.png'),
+  SlideInfo(
+      'Entrega rapida', 'Est mollit culpa irure do.', 'assetes/image/2.png'),
+  SlideInfo(
+      'Disfruta la camida',
+      'Nulla eiusmod Lorem reprehenderit non velit nisi.',
+      'assetes/image/3.png'),
+];
+
+class AppTutorialScreen extends StatelessWidget {
   static const name = 'tutorial_screen';
 
   const AppTutorialScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: PageView(
+        physics: const BouncingScrollPhysics(),
+        children: slides.map(
+          (slideData) => _Slide(
+            title: slideData.title, 
+            caption: slideData.caption, 
+            imageUrl: slideData.imageUrl
+            )
+            ).toList(),
+      ),
+    );
+  }
+}
+
+class _Slide extends StatelessWidget {
+  final String title;
+  final String caption;
+  final String imageUrl;
+
+  const _Slide({
+     required this.title, 
+     required this.caption, 
+     required this.imageUrl
+     });
 
   @override
   Widget build(BuildContext context) {
