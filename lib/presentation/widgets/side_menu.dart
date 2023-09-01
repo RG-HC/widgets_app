6e63267497cd2c4e90682a2d6b9cdbfddef9376e
RG-HC <al058785@ucam.mx>
2023-09-01 09:46:17 -0600

    Opciones notch y opciones menu

--- lib/presentation/widgets/side_menu.dart
+++ lib/presentation/widgets/side_menu.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:widgets_app/config/menu/menu_items.dart';
 
 class SideMenu extends StatefulWidget {
   const SideMenu({super.key});
@@ -12,6 +13,8 @@ class _SideMenuState extends State<SideMenu> {
 
   @override
   Widget build(BuildContext context) {
+    final hasNotch = MediaQuery.of(context).viewPadding.top > 35;
+
     return NavigationDrawer(
         selectedIndex: navDrawerIndex,
         onDestinationSelected: (value) {
@@ -20,12 +23,39 @@ class _SideMenuState extends State<SideMenu> {
           });
         },
         children: [
-          NavigationDrawerDestination(
-              icon: const Icon(Icons.sd_outlined),
-              label: const Text('Home Screen')),
-          NavigationDrawerDestination(
-              icon: const Icon(Icons.join_inner_rounded),
-              label: const Text('Otra Pantalla')),
+          Padding(
+            padding: EdgeInsets.fromLTRB(28, hasNotch ? 0 : 20, 16, 10),
+            child: const Text('Main'),
+          ),
+
+          ...appMenuItems
+          .sublist(0,3)
+          .map((item) => NavigationDrawerDestination(
+              icon: Icon(item.icon),
+              label: Text(item.title)
+              )
+              ),
+
+          const Padding(
+            padding: EdgeInsets.fromLTRB(28, 16, 16, 10),
+            child: Divider(),
+            ),
+
+
+            const Padding(
+            padding: EdgeInsets.fromLTRB(28, 10, 16, 10),
+            child: Text('More options'),
+            ),
+
+          ...appMenuItems
+          .sublist(3)
+          .map((item) => NavigationDrawerDestination(
+              icon: Icon(item.icon),
+              label: Text(item.title)
+              )
+              ),
+
+          
         ]);
   }
 }
