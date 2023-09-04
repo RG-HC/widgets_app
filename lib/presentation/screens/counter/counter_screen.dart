d444485654c77715a79079d2001588cc99237788
RG-HC <al058785@ucam.mx>
2023-09-04 13:07:21 -0600

    Iconos de light y dark mode

--- lib/presentation/screens/counter/counter_screen.dart
+++ lib/presentation/screens/counter/counter_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:widgets_app/presentation/providers/counter_provider.dart';
+import 'package:widgets_app/presentation/providers/theme_provider.dart';
 
 class CounterScreen extends ConsumerWidget {
   static const name = 'couter_screen';
@@ -10,10 +11,19 @@ class CounterScreen extends ConsumerWidget {
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     final int clickCounter = ref.watch(counterProvider);
+    final bool isDarkmode = ref.watch(isDarkmodeProvider);
 
     return Scaffold(
       appBar: AppBar(
         title: const Text('Couter Screen'),
+        actions: [
+          IconButton(
+              icon: Icon(isDarkmode? Icons.light_mode_outlined : Icons.dark_mode_outlined),
+              onPressed: () {
+                ref.read(isDarkmodeProvider.notifier)
+                .update((isDarkmode) => !isDarkmode);
+              })
+        ],
       ),
       body: Center(
         child: Text('Valor: $clickCounter',
